4eb003be24f4a2af14cc0567b4ce896fb87c5ad1
Steve Campbell <steve.campbell@q.com>
2012-06-25 08:48:06 -0600

    adding twitter research for Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResearch::Application.config.secret_token = '6b765bdd6f4797419567762344c47b10b04ad38542b1e2a6fc35df0cab33f3211982e4839a998864a1eb194cb35924b7d4b8b398dd19250967d7cd3b39b8884c'
